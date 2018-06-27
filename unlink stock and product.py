53c32ee6bacdc2129dd1942b081279a4feb7a643
Jigar <jam-odoo@users.noreply.github.com>
2018-06-27 12:38:02 -0700

    Create unlink stock and product.py

---
+++ unlink stock and product.py
@@ -0,0 +1,20 @@
+def get_chunks(l, n):
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
+env.cr.execute("UPDATE stock_move SET state='draft'")
+env.cr.execute("UPDATE stock_move_line SET state='draft'")
+env.cr.execute("UPDATE stock_picking SET state='draft'")
+env.cr.execute("UPDATE stock_inventory SET state='draft'")
+env.cr.commit()
+
+for model in ['product.public.category', 'mrp.bom', 'stock.move', 'stock.move.line', 'stock.picking', 'stock.inventory', 'stock.inventory.line', 'stock.quant']:
+    record_ids = env[model].search([])
+    for chunk in get_chunks(record_ids, 100):
+        chunk.unlink()
+        env.cr.commit()
+
+pp_ids = env['product.product'].search([])
+for x in get_chunks(pp_ids, 1000):
+    x.unlink()
+    env.cr.commit()
+  
