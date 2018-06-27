45d738e0594286de754ac3570db722a23806c0a9
Jigar <jam-odoo@users.noreply.github.com>
2018-06-27 12:44:38 -0700

    [ADD] more juice

--- unlink stock and product.py
+++ unlink stock and product.py
@@ -1,22 +1,24 @@
 #TRY THIS YOUR OWN RISK, NO GURANTEE OF WORKING :P
 # I hope you know what you are doing
+
 def get_chunks(l, n):
     for i in range(0, len(l), n):
         yield l[i:i + n]
+
 env.cr.execute("UPDATE stock_move SET state='draft'")
 env.cr.execute("UPDATE stock_move_line SET state='draft'")
 env.cr.execute("UPDATE stock_picking SET state='draft'")
 env.cr.execute("UPDATE stock_inventory SET state='draft'")
 env.cr.commit()
-
-for model in ['product.public.category', 'mrp.bom', 'stock.move', 'stock.move.line', 'stock.picking', 'stock.inventory', 'stock.inventory.line', 'stock.quant']:
+models = [
+    'product.public.category', 'mrp.bom', 'mrp.bom.line',
+    'stock.move', 'stock.move.line', 'stock.picking', 'stock.quant',
+    'stock.inventory', 'stock.inventory.line', 
+    'delivery.carrier', 'product.product'
+]
+for model in models:
     record_ids = env[model].search([])
-    for chunk in get_chunks(record_ids, 100):
+    #Lets not try to overlaod the server and  unlink records gracefully.
+    for chunk in get_chunks(record_ids, 1000):
         chunk.unlink()
         env.cr.commit()
-
-pp_ids = env['product.product'].search([])
-for x in get_chunks(pp_ids, 1000):
-    x.unlink()
-    env.cr.commit()
-  
