7802b759f97cb156c1becf645945621b33916cbe
Jigar <jam-odoo@users.noreply.github.com>
2018-07-09 09:39:32 -0700

    Create sweep_products.py

---
+++ sweep_products.py
@@ -0,0 +1,21 @@
+def get_chunks(l, n):
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
+
+env.cr.execute("UPDATE stock_move SET state='draft'")
+env.cr.execute("UPDATE stock_move_line SET state='draft'")
+env.cr.execute("UPDATE stock_picking SET state='draft'")
+env.cr.execute("UPDATE stock_inventory SET state='draft'")
+env.cr.commit()
+models = [
+    'product.public.category', 'mrp.bom', 'mrp.bom.line',
+    'stock.move', 'stock.move.line', 'stock.picking', 'stock.quant',
+    'stock.inventory', 'stock.inventory.line', 
+    'delivery.carrier', 'product.product'
+]
+for model in models:
+    record_ids = env[model].search([])
+    #Lets not try to overlaod the server and  unlink records gracefully.
+    for chunk in get_chunks(record_ids, 1000):
+        chunk.unlink()
+        env.cr.commit()
